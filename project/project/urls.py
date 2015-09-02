8440a501e1a8c246e07cb40c18186dbf52fa64d1
Michael Wisely <michaelwisely@gmail.com>
2015-09-02 03:39:16 -0500

    Start configuring allauth

--- project/project/urls.py
+++ project/project/urls.py
@@ -7,6 +7,8 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin_tools/', include('admin_tools.urls')),
+    url(r'^accounts/', include('allauth.urls')),
     url(r'^', include("project.teams.urls")),
     url(r'^', include("project.profiles.urls")),
 ]
