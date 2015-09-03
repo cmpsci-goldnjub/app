ccc98ced56ee8dda02332720c7146e1548a3b53c
Michael Wisely <michaelwisely@gmail.com>
2015-09-03 01:28:16 -0500

    Set up redirect to login view

--- project/project/urls.py
+++ project/project/urls.py
@@ -3,15 +3,20 @@ project URL Configuration
 
 """
 from django.conf.urls import include, url
+from django.conf import settings
 from django.contrib import admin
+from django.views.generic.base import RedirectView
+
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^admin_tools/', include('admin_tools.urls')),
 
+    url(r'^accounts/login/$', RedirectView.as_view(url=settings.LOGIN_URL),
+        name='account_login'),
     url(r'^accounts/logout/$', 'allauth.account.views.logout', name='account_logout'),
-    url('^accounts/social/', include('allauth.socialaccount.urls')),
-    url('^accounts/', include('allauth.socialaccount.providers.google.urls')),
+    url(r'^accounts/social/', include('allauth.socialaccount.urls')),
+    url(r'^accounts/', include('allauth.socialaccount.providers.google.urls')),
 
     url(r'^', include("project.teams.urls")),
     url(r'^', include("project.profiles.urls")),
