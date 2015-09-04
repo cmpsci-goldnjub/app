9c58f87648034d706ca5df87d7179226e48afa54
Michael Wisely <michaelwisely@gmail.com>
2015-09-04 10:30:43 -0500

    Use Django's username regex in profile URL

--- project/profiles/urls.py
+++ project/profiles/urls.py
@@ -12,5 +12,5 @@ from .views import (profile_list_view,
 urlpatterns = [
     url(r'^profiles/$', profile_list_view, name="profile_list"),
     url(r'^profiles/update/$', profile_update_view, name="profile_update"),
-    url(r'^profile/(?P<username>[-\w]+)/$', profile_detail_view, name="profile_detail"),
+    url(r'^profile/(?P<username>[\w.@+-]+)/$', profile_detail_view, name="profile_detail"),
 ]
