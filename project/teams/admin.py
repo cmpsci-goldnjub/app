7ba98c6b37b8707c32c9ab15f815b64c4bc45fc0
Michael Wisely <michaelwisely@gmail.com>
2015-09-04 11:08:11 -0500

    Use filter_horizontal for team admin

--- project/teams/admin.py
+++ project/teams/admin.py
@@ -6,7 +6,7 @@ from .models import Team, Request
 @admin.register(Team)
 class TeamAdmin(admin.ModelAdmin):
     model = Team
-
+    filter_horizontal = ('members',)
 
 @admin.register(Request)
 class RequestAdmin(admin.ModelAdmin):
