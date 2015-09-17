5ee6e876134fd2662edd76401763909f342cb87f
Michael Wisely <michaelwisely@gmail.com>
2015-09-16 20:24:47 -0500

    Use template for email

--- project/teams/views.py
+++ project/teams/views.py
@@ -1,11 +1,13 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.conf import settings
+from django.core.mail import send_mail
 from django.core.urlresolvers import reverse
 from django.shortcuts import redirect, get_object_or_404
+from django.template.loader import render_to_string
 from django.views.generic.edit import CreateView, UpdateView, FormView
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.core.mail import send_mail
 
 from .models import Team, Request, TeamFullException
 from .forms import ConfirmationForm
@@ -121,17 +123,18 @@ class RequestSendView(FormView):
         return context
 
     def form_valid(self, form):
-        Request.objects.create(team=self.team, user=self.request.user)
+        team_request = Request.objects.create(team=self.team, user=self.request.user)
 
         # Send email to team
-        team = Team.objects.get(name=self.team)
-
-        for user in team.members.all():
+        for user in self.team.members.all():
             if user.email:
-                subject = 'A User Requested to Join Your H4H Team!'
-                message = 'Dear {},\n\n{} has requested to join your Hackathon for Humanity team.  You can respond to their request by visiting your team page at http://h4h.mst.edu/teams/{}'.format(user, self.request.user, team.name)
+                subject = 'New H4H Team Request!'
+                message = render_to_string("teams/request_email.txt",
+                                           context={'request': team_request,
+                                                    'recipient': user})
 
-                send_mail(subject, message, 'roreply@h4h.mst.edu', [user.email], fail_silently=False)
+                send_mail(subject, message, settings.EMAIL_HOST_USER,
+                          [user.email], fail_silently=False)
 
         messages.success(self.request, "Request sent!")
         return super(RequestSendView, self).form_valid(form)
